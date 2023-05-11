b46a4741348c9ed820a574a46136cd01b96312c2
yerim <yerim@172.30.37.28>
2023-05-11 16:39:20 +0900

    git 생성

---
+++ src/test/java/com/ssp/closet/ClosetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssp.closet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClosetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
