dc1f41c256b62978e7451e1a463e873a66d231c1
s0104 <s0104@192.168.0.15>
2023-05-24 22:21:15 +0900

    Dao / Mapper 수정

--- src/main/java/com/ssp/closet/dao/mybatis/mapper/MeetMapper.java
+++ src/main/java/com/ssp/closet/dao/mybatis/mapper/MeetMapper.java
@@ -6,6 +6,8 @@ import com.ssp.closet.dto.Account;
 import com.ssp.closet.dto.Meet;
 
 public interface MeetMapper {
+	void insertMeet(Meet meet);
+	void deleteMeet(int meetId);
 	int countPeopleNum(int productId);
 	Meet getMeetDetail(int meetId);
 	List<Account> getMeetList(int productId);
