9f90552583d23adacab4f2001fe5ebb5bb34f037
Li5ht <bell1234795@gmail.com>
2023-06-04 17:12:12 +0900

    commit

--- src/main/java/com/ssp/closet/ClosetApplication.java
+++ src/main/java/com/ssp/closet/ClosetApplication.java
@@ -3,7 +3,7 @@ package com.ssp.closet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class ClosetApplication {
 
 	public static void main(String[] args) {
