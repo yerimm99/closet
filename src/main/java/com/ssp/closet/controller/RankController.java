b83157fd50b756e44644a0362437a87da6944a34
Li5ht <bell1234795@gmail.com>
2023-06-20 15:00:54 +0900

    일단 주석처리

--- src/main/java/com/ssp/closet/controller/RankController.java
+++ src/main/java/com/ssp/closet/controller/RankController.java
@@ -1,29 +1,25 @@
-package com.ssp.closet.controller;
-
-import com.ssp.closet.dto.Product;
-import com.ssp.closet.service.ClosetFacade;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
-
-@Controller
-public class RankController {
-    
-    private final ClosetFacade closetFacade;
-    
-    @Autowired
-    public RankController(ClosetFacade closetFacade) {
-        this.closetFacade = closetFacade;
-    }
-    
-    @GetMapping("/rank")
-    public String getRanking(Model model) {
-        List<Product> topRankingProducts = closetFacade.getTopRankingProducts();
-        model.addAttribute("topRankingProducts", topRankingProducts);
-        return "rank";
-    }
-}
+/*
+ * package com.ssp.closet.controller;
+ * 
+ * import com.ssp.closet.dto.Product; import
+ * com.ssp.closet.service.ClosetFacade;
+ * 
+ * import org.springframework.beans.factory.annotation.Autowired; import
+ * org.springframework.stereotype.Controller; import
+ * org.springframework.ui.Model; import
+ * org.springframework.web.bind.annotation.GetMapping;
+ * 
+ * import java.util.List;
+ * 
+ * @Controller public class RankController {
+ * 
+ * private final ClosetFacade closetFacade;
+ * 
+ * @Autowired public RankController(ClosetFacade closetFacade) {
+ * this.closetFacade = closetFacade; }
+ * 
+ * @GetMapping("/rank") public String getRanking(Model model) { List<Product>
+ * topRankingProducts = closetFacade.getTopRankingProducts();
+ * model.addAttribute("topRankingProducts", topRankingProducts); return "rank";
+ * } }
+ */
