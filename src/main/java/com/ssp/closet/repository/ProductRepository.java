45c8d8e8c7d344c54ba2fed842a0716d948342db
Li5ht <bell1234795@gmail.com>
2023-06-20 21:40:48 +0900

    Rank 관련 코드

--- src/main/java/com/ssp/closet/repository/ProductRepository.java
+++ src/main/java/com/ssp/closet/repository/ProductRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.ssp.closet.dto.Product;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Integer>{
 	//List<Product> findByOrderByRank();
 
@@ -37,6 +39,9 @@ public interface ProductRepository extends JpaRepository<Product, Integer>{
 		List<Product> findByPtypeAndStatus(int ptype, int status);
 		List<Product> findByNameIgnoreCaseContaining(String keywords);
 		//List<Product> findAll();
+		
+		@Query("SELECT p FROM Product p ORDER BY p.rank DESC")
+	    List<Product> findTopRankingProducts();
 		
 //		Product findByProductId(int productId);
 }
