a98cd9f50906d7b80c0554822efee520ceed8d94
Li5ht <89853141+Li5ht@users.noreply.github.com>
2023-06-22 16:04:13 +0900

    Update AccountRepository.java

--- src/main/java/com/ssp/closet/repository/AccountRepository.java
+++ src/main/java/com/ssp/closet/repository/AccountRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ssp.closet.dto.Account;
 
-public interface AccountRepository extends JpaRepository<Account, Integer> {
+public interface AccountRepository extends JpaRepository<Account, String> {
+
+	Optional<Account> findByUserId(String userId);
 
 }
